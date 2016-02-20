a3f5e7c2838c8bcc18b12c49ac3718b0b36bd2ca
Stefan Wendler <sw@kaltpost.de>
2016-02-20 22:49:56 +0100

    Use WifiManager.

--- firmware/arduino/main.cpp
+++ firmware/arduino/main.cpp
@@ -1,25 +1,30 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
+#include <DNSServer.h>
 
+#include "WiFiManager.h"
 #include "webserver.h"
 #include "gear.h"
 #include "range.h"
 #include "Adafruit_NeoPixel.h"
 
-const char* ssid = _SSID_;
-const char* password = _WIFI_PASSWORD_;
+// const char* ssid = _SSID_;
+// const char* password = _WIFI_PASSWORD_;
 
 MDNSResponder mdns;
 
 WebServer &server = WebServer::instance();
 Gear gear(5, 0, 4, 2);
 Range range(14, 12);
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(8, 10, NEO_GRB + NEO_KHZ800);
+// Adafruit_NeoPixel pixels = Adafruit_NeoPixel(8, 10, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(8, 10, NEO_GRB + NEO_KHZ800);
 
 
 void setup(void) {
 
     Serial.begin(115200);
+
+	/*
     WiFi.begin(ssid, password);
     Serial.println("");
 
@@ -34,6 +39,10 @@ void setup(void) {
     Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+	*/
+
+	WiFiManager wifiManager;
+	wifiManager.autoConnect("edubot", "edubot");
 
     if (mdns.begin(_MDNS_NAME_, WiFi.localIP())) {
         Serial.println("MDNS responder started");
